1a5c3c24c71d4f6017eb8eefe91f69be849ca952
voidermalie <lestarfanny@gmail.com>
2024-04-11 15:58:51 +0200

    username display fix

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,9 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import TaskList from '../../components/TaskList/TaskList';
 import { selectUser } from '../../store/selectors';
+import { fetchUser } from '../../store/Slice/userSlice';
+import { useEffect } from 'react';
 
 const TaskPage = () => {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, []);
 
   return (
     <div>
--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -2,5 +2,5 @@ export const selectTaskTitle = state => state.tasks.task.title
 
 export const selectTasks = state => state.tasks.tasks
 
-export const selectUser = state => state.user.userName
+export const selectUser = state => state.user.username
 export const selectUserStatus = state => state.user.status
